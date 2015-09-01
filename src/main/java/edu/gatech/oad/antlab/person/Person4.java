3350a951a2e4c67f9de21907d246089ba3e8dbe2
jhoffman1 <jhoffman40@gatech.edu>
2015-09-01 16:18:58 -0400

    Person 4 method completed

--- src/main/java/edu/gatech/oad/antlab/person/Person4.java
+++ src/main/java/edu/gatech/oad/antlab/person/Person4.java
@@ -31,7 +31,16 @@ public class Person4 {
      */
     private String calc(String input) {
       //Person 4 put your implementation here
-      return null;
+      String output = "";
+      int ch;
+      char c;
+      for (int i = 0; i < input.length(); i++) {
+        ch = input.charAt(i);
+        ch++;
+        c = (char)ch;
+        output += c;
+      }
+      return output;
     }
     
     /**
